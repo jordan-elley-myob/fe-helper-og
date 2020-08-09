cf9a6ebf08a9fefccc179d47c731d5fc614a6706
jordan elley <jordan.elley@myob.com>
2020-08-09 22:56:15 +1200

    added suggestions for source aswell as model and layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
 import React from 'react';
 import './App.css';
-import { TextArea} from "@myob/myob-widgets";
+import {TextArea} from "@myob/myob-widgets";
+
+
 function camelize(str) {
   return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
     return index === 0 ? word.toLowerCase() : word.toUpperCase();
   }).replace(/\s+/g, '');
 }
 
+function makeModel(textRaw){
+  const lines = textRaw.split("\n");
+  const layoutChildren = [(<h2>Some Entrys for model which might be useful</h2>)];
+  for (let i = 0; i < lines.length; i+=2) {
+    const id = camelize(lines[i]);
+    const child = (
+        <div>
+          <p>{id}: <br/>
+          type: number</p>
+    </div>)
+    layoutChildren.push(child)
+  }
+  layoutChildren.push(<h2>Other suggestions</h2>)
+  for (let i = 1; i < lines.length; i+=2) {
+    const id =lines[i];
+    const child = (
+        <div>
+          <p>{id}: <br/>
+            type: number</p>
+        </div>)
+    layoutChildren.push(child)
+  }
+  layoutChildren.push(<h2>Source suggestions</h2>)
+
+  for (let i = 1; i < lines.length; i+=2) {
+    const id =lines[i];
+    const child = (
+        <div>
+          <p>{id}: <br/>
+            source: forms/tax-forms-nz.*/{id}</p>
+        </div>)
+    layoutChildren.push(child)
+  }
+
+  return layoutChildren
+}
+
+//make an option to set manual Id
 function makeLayout(textRaw){
   const lines = textRaw.split("\n");
-  var sectionChildren = <div/>;
+  const layoutChildren = [(<h1>Layout Children</h1>)];
   for (let i = 0; i < lines.length; i+=2){
-    sectionChildren+=<div>
-      <h1>Layout Child</h1>
-      <p>- id: {camelize(lines[i])} <br/>
-        label: {lines[i]}<br/>
-        format: '(1,234.10)'<br/>
-        visible: =IF(OR({lines[i]}=0,{lines[i]}=NULL),FALSE,TRUE)</p>
-    </div>
-
+    const id = camelize(lines[i])
+    const child =(<div>
+         <p>- id: {id} <br/>
+           label: {lines[i]}<br/>
+           format: '(1,234.10)'<br/>
+           visible: =IF(OR({id}=0,{id}=NULL),FALSE,TRUE)</p>
+       </div>)
+    layoutChildren.push(child);
   }
-  // return (<div>
-  //   <h1>Layout Child</h1>
-  //   <p>- id: {camelize(lines[0])} <br/>
-  //     label: {lines[0]}<br/>
-  //     format: '(1,234.10)'<br/>
-  //     visible: =IF(OR({lines[0]}=0,{lines[0]}=NULL),FALSE,TRUE)</p>
-  // </div>)
-
-  return (sectionChildren)
+  return layoutChildren
 }
 
 function App() {
@@ -41,13 +73,20 @@ function App() {
             onChange={(event) => {onTextAreaChange(event.target.value)}}
             value={textArea || ''}
             name="default"
-            label="Dynamic List Data"
-            rows={8}
+            label="Dynamic List Data- Label- Amount"
+            rows={20}
             white-space='pre-wrap'
         />
 
         <div>
-          {makeLayout(textArea)}
+          {
+            makeLayout(textArea.replace(/^\s*\n/gm,'')) //removes empty lines
+          }
+        </div>
+        <div>
+          {
+            makeModel(textArea.replace(/^\s*\n/gm,'')) //removes empty lines
+          }
         </div>
       </header>
     </div>
